3082e29af747be4ed5c58ee2f7da3ef7ba9e87c3
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-03 23:39:41 +0900

    mathモジュール利用スクリプトを追加

---
+++ Python/Chapter02/progbp02-1.py
@@ -0,0 +1,14 @@
+import math
+
+def main():
+
+    real = math.sqrt(3.0)/2.0
+    imag = 0.5
+
+    angle = math.atan2(imag, real)*180.0/math.pi
+
+    print(angle)
+
+if __name__ == '__main__':
+
+    main()
