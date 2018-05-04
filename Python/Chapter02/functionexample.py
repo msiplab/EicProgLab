1909d1763b575320aa8520817f3feb0674dd657f
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-04 11:32:42 +0900

    関数定義の例を追加

---
+++ Python/Chapter02/functionexample.py
@@ -0,0 +1,12 @@
+import sys
+
+def main(args):
+	a = int(args[1])
+	b = int(args[2])
+	print('a+b= ' + str(mysum(a,b)))
+
+def mysum(a,b):
+	return a+b
+
+if __name__ == '__main__':
+	main(sys.argv)
