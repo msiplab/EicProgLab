e91d22cfc23224a1f50318e1bf1546b1b6d9eda2
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-04 22:33:41 +0900

    メソッド，コンストラクタの例

--- Python/Chapter02/MyComplex.py
+++ Python/Chapter02/MyComplex.py
@@ -1,4 +1,4 @@
-import math
+# import math
 
 class MyComplex:
 	'''複素数クラス'''
@@ -7,9 +7,24 @@ class MyComplex:
 	real = float()
 	imag = float()
 	
+	"""
+	def __init__(self, real, imag):
+		'''コンストラクタ'''
+		# フィールドの初期化
+		self.real, self.imag = real, imag
+	"""
+	
+	"""
 	def display(self):
 		'''複素数表示メソッド'''
 		
 		s = '+j' if self.imag > 0 else '-j'
 		print(str(self.real)+s+str(math.fabs(self.imag)))
-
+    """
+    
+    """
+	def cabs(self):
+		'''絶対値メソッド'''
+		
+		return math.sqrt(self.real**2 + self.imag**2)
+    """
