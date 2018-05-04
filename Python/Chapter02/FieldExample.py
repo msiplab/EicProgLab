fd28273ed1ffe8da2f320cb1ec826ca575b0fb3a
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-04 17:09:33 +0900

    メソッドおよびフィールドの利用例を追加

---
+++ Python/Chapter02/FieldExample.py
@@ -0,0 +1,26 @@
+'''メソッドの例'''
+import math
+
+class MyVector:
+	'''ベクトルクラス'''
+	
+	def __init__(self,x,y,z):
+		'''初期化メソッド（コンストラクタ）'''
+		self._x, self._y, self._z = x, y, z
+
+		
+	def norm(self):
+		'''ノルムの計算'''
+		x, y, z = self._x, self._y, self._z
+		
+		# 自乗和の平方根		
+		return math.sqrt(x**2 + y**2 + z**2) 
+
+if __name__ == '__main__':
+	
+	import FieldExample as fe
+	
+	vector = fe.MyVector(1., 2., 3.)
+	
+	print(vector.norm())
+		
---
+++ Python/Chapter02/MethodExample.py
@@ -0,0 +1,19 @@
+'''メソッドの例'''
+import math
+
+class MyVector:
+	'''ベクトルクラス'''
+	
+	def norm(self,x,y,z):
+		'''ノルムの計算'''
+		
+		return math.sqrt(x**2 + y**2 + z**2) # 自乗和の平方根
+
+if __name__ == '__main__':
+	
+	import MethodExample as me
+	
+	vector = me.MyVector()
+	
+	print(vector.norm(1., 2., 3.))
+		
