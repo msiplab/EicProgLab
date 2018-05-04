c7bb302fe416d859079c5bceb7ff4a4fed549ce9
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-04 13:55:16 +0900

    Update progex01-2.py

--- Python/Chapter01/progex01-2.py
+++ Python/Chapter01/progex01-2.py
@@ -9,7 +9,7 @@ def main(args):
 	# 引数の数
 	nArgs = len(args)-1
 	if nArgs != 1:
-		print('エラー:引数の数 ',nArgs)
+		print('エラー:引数の数 ', nArgs)
 	else:
 		# コマンド引数からのデータの読み込み（C言語の atof に相当）
 		ell = float(args[1])
@@ -18,8 +18,8 @@ def main(args):
 		area = ell * (SQRT_OF_3 / 2.0 * ell) / 2.0
 		
 		# 正三角形の面積の表示 
-		print('一辺の長さが ',ell,' の正三角形の面積')
-		print('     = ',area)
+		print('一辺の長さが ', ell,' の正三角形の面積')
+		print('     = ', area)
 	
 	return 0
 
