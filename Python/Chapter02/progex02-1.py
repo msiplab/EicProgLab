4587eead74a2c23942381c1aad4871854ba334a3
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-04 14:17:31 +0900

    配列の利用例を追加

---
+++ Python/Chapter02/progex02-1.py
@@ -0,0 +1,14 @@
+def main():
+	
+	a123 = [1, 2, 3]
+	
+	for i in range(len(a123)):
+		
+		print(a123[i], ' ',end='')
+		
+	print()
+
+
+if __name__ == '__main__':
+	
+	main()
---
+++ Python/Chapter02/progex02-2.py
@@ -0,0 +1,17 @@
+def main():
+	
+	a = [ 	[ 1.0 ],
+			[ 1.0, 2.0],
+			[ 1.0, 2.0, 3.0] ]
+	
+	for iRow in range(len(a)):
+		
+		for iCol in range(len(a[iRow])):
+			
+			print(a[iRow][iCol], ' ', end='')
+			
+		print()
+
+if __name__ == '__main__':
+	
+	main()
