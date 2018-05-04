3ee0a5286d5119690c08609b650cd1e4ee58a682
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-04 18:26:03 +0900

    インスタンス化の例を追加

---
+++ Python/Chapter02/MyComplex.py
@@ -0,0 +1,5 @@
+class MyComplex:
+	'''複素数クラス'''
+	
+	real = float()
+	imag = float()
---
+++ Python/Chapter02/progex02-4.py
@@ -0,0 +1,22 @@
+import MyComplex as mc
+import math
+import sys
+
+def main(args):
+	
+	z = mc.MyComplex()
+	
+	z.real, z.imag = float(args[1]), float(args[2])
+	
+	display(z)
+
+
+def display(z):
+	'''複素数表示関数'''
+	s = '+j' if z.imag > 0 else '-j'
+	print(str(z.real)+s+str(math.fabs(z.imag)))
+
+if __name__ == '__main__':
+	
+	main(sys.argv)
+	
