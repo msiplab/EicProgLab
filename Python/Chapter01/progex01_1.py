00c1c6558d1d4e328ae975e68e307a84ffe3713d
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-13 18:40:22 +0900

    タブを空白に修正

--- Python/Chapter01/progex01_1.py
+++ Python/Chapter01/progex01_1.py
@@ -1,7 +1,7 @@
-maxValue = 10
+maxvalue = 10
 sum = 0
 
-for value in range(0,maxValue+1):
-	sum += value
+for value in range(0,maxvalue+1):
+    sum += value
 
 print(sum)
--- Python/Chapter01/progex01_2.py
+++ Python/Chapter01/progex01_2.py
@@ -8,24 +8,24 @@ import sys
 SQRT_OF_3 = 1.73205080756888
 
 def main(args):
-	# 引数の数
-	nArgs = len(args)-1
-	if nArgs != 1:
-		print('エラー:引数の数 ', nArgs)
-	else:
-		# コマンド引数からのデータの読み込み（C言語の atof に相当）
-		ell = float(args[1])
-		
-		# 面積の計算(底辺×高さ/2) 
-		area = ell * (SQRT_OF_3 / 2.0 * ell) / 2.0
-		
-		# 正三角形の面積の表示 
-		print('一辺の長さが ', ell,' の正三角形の面積')
-		print('     = ', area)
-	
-	return 0
+    # 引数の数
+    nargs = len(args)-1
+    if nargs != 1:
+        print('エラー:引数の数 ', nargs)
+    else:
+        # コマンド引数からのデータの読み込み（C言語の atof に相当）
+        ell = float(args[1])
+        
+        # 面積の計算(底辺×高さ/2) 
+        area = ell * (SQRT_OF_3 / 2.0 * ell) / 2.0
+        
+        # 正三角形の面積の表示 
+        print('一辺の長さが ', ell,' の正三角形の面積')
+        print('     = ', area)
+    
+    return 0
 
      
 if __name__ == '__main__':
-	
-	main(sys.argv)
+    
+    main(sys.argv)
