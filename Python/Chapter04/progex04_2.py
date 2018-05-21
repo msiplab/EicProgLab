f7af830da0abc0f3cf54fa9a4bf4dcafc1bbc401
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-21 14:46:53 +0900

    標準入出力の例を追加

---
+++ Python/Chapter04/progex04_2.py
@@ -0,0 +1,17 @@
+import sys
+
+def main():
+    # 標準出力
+    sys.stdout.write('Input a character: ')
+    sys.stdout.flush()
+    # 標準入力
+    c = sys.stdin.read(1)
+    sys.stdout.write("character '" + c 
+                        + "' has a code number "
+                        + str(ord(c)) + '.\n')
+    sys.stdout.flush()
+    # 標準エラー
+    sys.stderr
+    
+if __name__ == '__main__':
+    main()
