fbb9286ddb43860848fcc176895f01ed33d2e6e1
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-22 10:23:34 +0900

    例外処理の例を追加

---
+++ Python/Chapter04/progex04_4.py
@@ -0,0 +1,19 @@
+import sys
+def main(args):
+    try:
+        nom = int(args[1])  # 分子の読込み
+        den = int(args[2])  # 分母の読込み
+        print('{0:d}/{1:d} = {2:d} 余り {3:d}'.format(
+                nom, den, nom//den, nom%den))
+    except ZeroDivisionError:
+        print('ゼロ割が発生しました。')
+    except IndexError:
+        print('引数が足りません。')
+    except ValueError:
+        print('整数ではありません。')
+    else:
+        print('例外処理は発生しませんでした。')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
