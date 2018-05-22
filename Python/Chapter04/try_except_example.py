bab0495791b79e7dae53b0382d7bc6abf73c3b20
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-22 09:44:46 +0900

    例外処理の例を追加

---
+++ Python/Chapter04/try_except_example.py
@@ -0,0 +1,18 @@
+import sys
+
+def main():
+    # 名前の入力
+    name = input('名前を入力してください> ')
+    # 年齢の入力（整数が入力されるまで問い続ける）
+    while (True):
+        try:
+            age = int(input('年齢を入力してください> '))
+            break
+        except ValueError:
+            print('年齢には整数を入力してください！')
+    # 名前と年齢の表示
+    print('名前：{0} （{1:d}歳）'.format(name,age))
+
+
+if __name__ == '__main__':
+    main()
