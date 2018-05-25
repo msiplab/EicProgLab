dacba71e96404707824c61d3a2c517a7c5688a43
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-05-25 19:48:58 +0900

    コメントの追記

--- Python/Examples1/example2_1.py
+++ Python/Examples1/example2_1.py
@@ -1,25 +1,37 @@
+'''
+example2_1
+
+例題2-1（基礎）
+
+Copyright (c) 2018 Shogo MURAMATSU, All rights reserved
+'''
 import othello
 
 def main():
     try:
+        # Board型のインスタンスboardを生成
         board = othello.Board()
-        
+        # 盤(board)の状態を表示（初期状態）
         board.displayState()
-        
+        # 盤(board)の3列4行目に手番の石(Black)を置いてみる
         print('(x,y) = (3,4)')
         board.tryPlaceStone(3,4)
-        
+        # 盤(board)の状態を表示
         board.displayState()
         
+        # 盤(board)の5列3行目に手番の石(White)を置いてみる
         print('(x,y) = (5,3)')
         board.tryPlaceStone(5,3)
-        
+        # 盤(board)の状態を表示
         board.displayState()
         
+        # パスする
         print('パス')
         board.change()
-        
+        # 盤(board)の状態を表示
         board.displayState()
+        
+        print()
         
     except othello.BoardOutOfRangeException as boore:
         print()
--- Python/Examples1/othello.py
+++ Python/Examples1/othello.py
@@ -1,38 +1,60 @@
 class PointState:
+    '''マス目の状態クラス'''
+    
     def __init__(self, mark):
+        '''コンストラクタ'''
         self.__mark = mark
+        
     def __str__(self):
+        '''文字列への変換'''
         return self.__mark 
 
+
 class BoardOutOfRangeException(Exception):
-    def __init__(self, e):
-        Exception.__init__(self, e)
+    '''盤外指定時の例外クラス（Exceptionの子クラス）'''
+    
+    def __init__(self, msg):
+        Exception.__init__(self, msg)
+        
 
 class Board:
-
+    '''リバーシ（オセロ）の盤クラス'''
+    
+    # ここクラスでしか使わない定数
     __NONE = PointState('.')
     __BLACK_STONE = PointState('B')
     __WHITE_STONE = PointState('W')
 
     def __init__(self):
-        self.__currentStone = Board.__BLACK_STONE
+        '''コンストラクタ'''        
+        # 最初の手番を黒に設定
+        self.__currentStone = Board.__BLACK_STONE        
+        # 盤上の石をすべてクリア(_NONEと設定
         self.__stateArray = [ [ Board.__NONE
                                 for irow in range(1,9) ]
-                                for icol in range(1,9) ]
+                                for icol in range(1,9) ]        
+        # 盤を初期状態にするために石を置く
         self.__setState(4, 4, Board.__WHITE_STONE)
         self.__setState(5, 4, Board.__BLACK_STONE)
         self.__setState(4, 5, Board.__BLACK_STONE)
         self.__setState(5, 5, Board.__WHITE_STONE)
+        
 
     def displayState(self):
+        '''盤の状態表示'''
         try:
-            print('\n 12345678')
+            # 列番号の表示
+            print('\n 12345678') 
             for y in range(1,9):
+                # 行番号の表示
                 print(y, end='')
                 for x in range(1,9):
+                    # x列目，y行目のマスの状態
                     state = self._getState(x, y)
                     print(state,end='')
                 print()
+            print()
+            # 現在の手番の色の表示
             print('現在の手番：',end='')
             if self.__currentStone == Board.__BLACK_STONE:
                 print('黒(B)')
@@ -42,11 +64,18 @@ class Board:
             print(boore.message)
 
     def change(self):
+        '''パス'''
         self.__currentStone = self.__getEnemyStone()
 
     def tryPlaceStone(self, x, y):
+        '''
+        座標(x,y)に石を試しに置く。
+        石を置こうとする場所が盤の範囲外であれば例外を投げる。
+        '''
+        # 例外を萎える可能性あり
         if self._getState(x,y) != Board.__NONE:
             return False
+        # 石を置こうとする場所の隣を返せるかどうかを確認
         trial = False
         trial = trial | self.__tryReverseNext(x, y, +1,  0) # 右
         trial = trial | self.__tryReverseNext(x, y,  0, +1) # 下
@@ -56,21 +85,30 @@ class Board:
         trial = trial | self.__tryReverseNext(x, y, +1, -1) # 右上
         trial = trial | self.__tryReverseNext(x, y, -1, +1) # 左下
         trial = trial | self.__tryReverseNext(x, y, -1, -1) # 上上
-        if trial: # 隣の石を返すことができる場合
+        # 隣の石を返すことができる場合
+        if trial: 
             # 石を置いてボードの状態を更新する。
             self.__setState(x, y, self.__currentStone)
             # 手番を相手に渡す
             self.change()
             
     def _getState(self, x, y):
+        '''指定した位置のマスの状態を取得'''
+        
+        # マスの範囲外であれば例外を投げる
         if x < 1 or x > 8 or y < 1 or y > 8:
-            raise BoardOutOfRangeException('xとyは、0以上かつ9以下の値でなければなりません。')
+            raise BoardOutOfRangeException(
+                       'xとyは、0以上かつ9以下の値でなければなりません。')
+        # 盤のx列目，y行目の状態をリターン
         return self.__stateArray[x-1][y-1]
     
     def __tryReverseNext(self, x, y, dx, dy):
         try:
+            # 隣の石が相手の石であるかどうかの確認
             if self._getState(x+dx, y+dy) == self.__getEnemyStone():
+                # 隣の隣の石が自分の石であるかどうかの確認
                 if self._getState(x+dx+dx,y+dy+dy) == self.__currentStone:
+                    # 隣の石が相手で，隣の隣の石が自分である場合，石を返して盤を更新
                     self.__setState(x+dx, y+dy, self.__currentStone)
                     return True
                 elif self.__tryReverseNext(x+dx, y+dy, dx, dy):
@@ -81,12 +119,15 @@ class Board:
             else:
                 return False
         except BoardOutOfRangeException as boore:
+            # 盤の範囲外にアクセスがあれば失敗
             return False
     
     def __setState(self, x, y, state):
+        '''盤のx列目y行目のマスの状態を与えられた状態に更新'''
         self.__stateArray[x-1][y-1] = state
 
     def __getEnemyStone(self):
+        '''相手（次の手番）の色を取得'''
         if self.__currentStone == Board.__BLACK_STONE:
             return Board.__WHITE_STONE
         else:
