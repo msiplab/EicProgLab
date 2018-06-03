9d4560af9b6424b0d9aa13e7359cb33dae11137c
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-06-03 16:15:55 +0900

    多態性の例を追加

--- Python/Chapter05/polymorphism_example.py
+++ Python/Chapter05/polymorphism_example.py
@@ -1,10 +1,14 @@
 from digital_tv import DigitalTv
 from digital_tv_3d import DigitalTv3d
 
-if __name__ == '__main__':
+def main():
     region = '新潟'
-    digitaltv = DigitalTv(region)
-    digitaltv.display(8)
-    
-    digitaltv = DigitalTv3d(region)
-    digitaltv.display(8)
+    for flag in [ True, False]:
+        if flag == True:
+            digitaltv = DigitalTv3d(region)
+        else:
+            digitaltv = DigitalTv(region)
+        digitaltv.display(8)
+
+if __name__ == '__main__':
+    main()
