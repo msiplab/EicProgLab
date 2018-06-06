973443402f90e480c67fad25a32bbc63d43f7d37
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-06-06 09:41:43 +0900

    インタフェースの例を追加

--- Python/Chapter05/interface_example.py
+++ Python/Chapter05/interface_example.py
@@ -4,17 +4,17 @@ from lcd_tv import LcdTv
 from tv_exception import TvException
 
 def main(args):
-    tvSpec = args[1]
+    tvspec = args[1]
     region = args[2]
     channel = int(args[3])
     
     try:
-        if tvSpec == 'プラズマ':
+        if tvspec == 'プラズマ':
             tv = PlasmaTv()
-        elif tvSpec == '液晶':
+        elif tvspec == '液晶':
             tv = LcdTv()
         else:
-            raise TvException(tvSpec + 'は利用不可．')
+            raise TvException(tvspec + 'は利用不可．')
         tv.region = region
         tv.display(channel)
     except TvException as te:
