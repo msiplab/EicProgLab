397885e708bab3a36ebe24632b18228cfa96c141
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-06-17 10:18:34 +0900

    tkinter利用例の修正

--- Python/Chapter05/tkinter_example.py
+++ Python/Chapter05/tkinter_example.py
@@ -1,13 +1,21 @@
 import tkinter as tk
 
-class MyLabel(tk.Label):
+class MyLabel(tk.Frame):
     
-    def __init__(self, msg):
-        super().__init__(frame=None, 
-                            text=msg, 
-                            font=('Helvetica','24','bold'))
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.label = tk.Label(self)
+        self.label['text'] = 'Ciao, Mondo!'
+        self.label['font'] = ('Helvetica','24','bold')
+        self.label.pack()
 
 if __name__ == '__main__':
-    label = MyLabel('Ciao, Mondo!')
-    label.pack()
-    label.mainloop()
+    root = tk.Tk()
+    mylabel = MyLabel(root)
+    mylabel.mainloop()
+    
+
