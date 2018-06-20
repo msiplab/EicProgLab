08ab8dc5b1f33f770046d687f52f04c7ed90adab
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-06-21 00:05:06 +0900

    標準入出力の例

---
+++ Python/Chapter06/matrix_transposition_stdio.py
@@ -0,0 +1,20 @@
+import re
+
+def main():
+    # 配列の初期化
+    matrix = [ [ 0 for irow in range(2) ] for icol in range(2) ]
+    # 配列要素の入力  
+    for irow in range(2):
+        rowvector = input()
+        rowelements = re.split('\s*,?\s*', rowvector)
+        for icol in range(2):
+            matrix[irow][icol] = float(rowelements[icol])
+    # 配列要素の転置出力
+    for irow in range(2):
+        for icol in range(2):
+            send = '' if icol == 1 else ', '
+            print('{0:.2f}'.format(matrix[icol][irow]), end=send)
+        print()
+
+if __name__ == '__main__':
+    main()
