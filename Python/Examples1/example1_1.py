f7f13b2bc5b24d5c4b369e6c6039909c948209a3
shodimaggio <shogo@eng.niigata-u.ac.jp>
2022-06-11 14:14:50 +0900

    2022年度ように更新

--- Python/Examples1/example1_1.py
+++ Python/Examples1/example1_1.py
@@ -1,22 +1,32 @@
 """
 example1_1.py
-(c) Copyright 2018-2022, Shogo MURAMATSU, All rights reserved
+(c) Copyright 2022, Shogo MURAMATSU, All rights reserved
 """
 import sys
 
-SQRT_OF_3 = 1.73205080756888
+# 電源電圧値
+SRC_VOLTAGE = 5.0
+# 抵抗の数
+NUM_REGS = 5
 
 def main(args):
-    if len(args) != 2:
+    if len(args) != 6:
         print('エラー：引数の数' + str(len(args)))
     else:
-        # 一辺の長さ
-        ell = float(args[1])
-        # 面積の計算（底辺×高さ/2）
-        area = ell * (SQRT_OF_3/2.0 * ell)/2.0
-        # 正三角形の面積の表示
-        print('一辺の長さが' + str(ell) + 'の正三角形の面積')
-        print('    = ' + str(area))
+        # 抵抗値 Ri
+        r = [ 0.0 for idx in range(NUM_REGS) ]
+        for idx in range(NUM_REGS):
+            r[idx] = float(args[idx+1])
+        # 抵抗R0にかかる電圧V
+        v0 = (r[2]*r[3]-r[1]*r[4])*r[0]*SRC_VOLTAGE / \
+            ( r[1]*r[3]*(r[2]+r[4]) + \
+              r[2]*r[4]*(r[1]+r[3]) + \
+              r[0]*(r[1]+r[3])*(r[2]*r[4]) )            
+        # 電圧Vの表示
+        print('電源電圧 E = ' + str(SRC_VOLTAGE) + ' V')
+        for idx in range(NUM_REGS):
+            print('抵抗 R' + str(idx) + ' = ' + str(r[idx]) + ' Ω')
+        print('電圧 V0 = ' + str(v0) + ' V')
 
 if __name__ == '__main__':
     main(sys.argv)
