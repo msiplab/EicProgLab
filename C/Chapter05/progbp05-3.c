5c2ed34c61cd6d0046d602c3c281d91f8d6a2ff3
shodimaggio <shogo@eng.niigata-u.ac.jp>
2023-05-12 10:05:08 +0900

    ファイル名を修正

--- C/Chapter05/progbp05-3.c
+++ C/Chapter05/progbp05-3.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 /* 関数プロトタイプ */
-void sumValue(int a, int b, int sum); 
+void sumPointer(int a, int b, int* pSum);
 
 int main() {
   int a = 1;
   int b = 2;
   int sum = 0;
 
-  /* 値呼出しの関数を実行 */
-  sumValue(a,b,sum);
+  /* 参照呼出しの関数を実行 */
+  sumPointer(a,b,&sum);
 
   /* sum の値は？ */
-  printf("%d + %d = %d ?\n", a, b, sum);
+  printf("%d + %d = %d !\n", a, b, sum);
   
   return 0;
 
 }
 
-void sumValue(int a, int b, int sum) {
-  
+void sumPointer(int a, int b, int* pSum) {
+
   /* 足し算して値を更新 */
-  sum = a + b; 
+  *pSum = a + b; 
 
 }
